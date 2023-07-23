bcbe621edcf8e84cdf34038728d851a871535ecf
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-07-23 12:11:58 -0400

    Spring initializr

---
+++ src/test/java/com/sg/FinancialManagementSystem/FinancialManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sg.FinancialManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
