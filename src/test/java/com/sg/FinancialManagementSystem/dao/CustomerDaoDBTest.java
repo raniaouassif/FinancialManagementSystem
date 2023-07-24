7cf8f579adcc017ec77f9dcd370e59881f95c284
Rania Ouassif <rania.ouassif@mail.mcgill.ca>
2023-07-24 01:56:45 -0400

    generated test customerDaoDB

---
+++ src/test/java/com/sg/FinancialManagementSystem/dao/CustomerDaoDBTest.java
@@ -0,0 +1,43 @@
+package com.sg.FinancialManagementSystem.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author raniaouassif on 2023-07-24
+ */
+class CustomerDaoDBTest {
+
+    @Test
+    void getCustomerByID() {
+    }
+
+    @Test
+    void getAllCustomers() {
+    }
+
+    @Test
+    void addCustomer() {
+    }
+
+    @Test
+    void updateCustomer() {
+    }
+
+    @Test
+    void deleteCustomerByID() {
+    }
+
+    @Test
+    void getCustomersByBank() {
+    }
+
+    @Test
+    void getCustomersByAccountType() {
+    }
+
+    @Test
+    void getCustomersByStock() {
+    }
+}
